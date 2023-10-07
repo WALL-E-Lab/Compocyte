c5af3e6cdfada967ccf5f8c22d269b705b2ea6d0
Christopher Beltz <sandmensch933@web.de>
2023-10-07 17:14:47 +0200

    Fixes for log reg saving, loading, prediction (activation augmentation)

--- src/scFlorist/core/models/log_reg.py
+++ src/scFlorist/core/models/log_reg.py
@@ -7,8 +7,10 @@ class LogisticRegression():
 
     possible_data_types = ['counts', 'normlog']
 
-    def __init__(self, data_type=None, model=None, C=1.0, solver='sag', max_iter=1000, n_jobs=None, logreg_kwargs={}, **kwargs):
-        self.fixed = None
+    def __init__(self, n_output, data_type=None, fixed=None, model=None, C=1.0, solver='sag', max_iter=1000, n_jobs=None, logreg_kwargs={}, **kwargs):
+        self.fixed = fixed
+        self.n_output = n_output
+        self.data_type = data_type
         if model is None:
             self.model = LogReg (
                 C=C, 
@@ -34,7 +36,7 @@ class LogisticRegression():
         # Cannot train logreg model with only one class present
         # Most sensible approach is to set all predictions in the future to the only learned class
         if len(np.unique(y_int)) == 1:
-            self.fixed = 0
+            self.fixed = np.unique(y_int)[0]
 
         else:
             self.model.fit(x, y_int)
@@ -42,27 +44,39 @@ class LogisticRegression():
     def predict(self, X):
         if self.fixed is None:
             pred_activations = self.model.predict_proba(X)
+            # Augment prediction matrix if one of the child classes
+            # was not present in training data to avoid a mismatch
+            # between the idx in prediction matrix and the i in
+            # for label encoding
+            if len(self.model.classes_) != self.n_output:
+                augmented_pred_activations = np.zeros(
+                    shape=(len(pred_activations), self.n_output))
+                for idx, label_i in enumerate(self.model.classes_):
+                    activations_class = pred_activations[:, idx]
+                    augmented_pred_activations[:, label_i] = activations_class
+
+                return augmented_pred_activations
 
         else:
-            pred_activations = np.ones(shape=(len(X), 1))
+            pred_activations = np.zeros(
+                    shape=(len(X), self.n_output))
+            pred_activations[:, self.fixed] = 1
             
         return pred_activations
 
     def _save(self, path):
-        with open(os.path.join(path, 'model.pickle'), 'wb') as f:
-            pickle.dump(self.model, f)
-
-        with open(os.path.join(path, 'data_type.pickle'), 'wb') as f:
-            pickle.dump(self.data_type, f)
+        for attribute in ['model', 'data_type', 'n_output', 'fixed']:
+            with open(os.path.join(path, f'{attribute}.pickle'), 'wb') as f:
+                pickle.dump(
+                    getattr(self, attribute, None), f)
 
     @classmethod
     def _load(cls, path):
-        with open(os.path.join(path, 'model.pickle'), 'rb') as f:
-            model = pickle.load(f)
+        args = {}
+        for attribute in ['model', 'data_type', 'n_output', 'fixed']:
+            with open(os.path.join(path, f'{attribute}.pickle'), 'rb') as f:
+                args[attribute] = pickle.load(f)
 
-        with open(os.path.join(path, 'data_type.pickle'), 'rb') as f:
-            data_type = pickle.load(f)
-
-        log_reg = cls(model=model, data_type=data_type)
+        log_reg = cls(**args)
 
         return log_reg
