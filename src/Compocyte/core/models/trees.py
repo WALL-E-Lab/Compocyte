0c97553a7f213b47d423e47796a1d58e8a18c607
Christopher Beltz <sandmensch933@web.de>
2025-03-22 15:44:32 +0100

    label consistency in classification with trees

--- src/Compocyte/core/models/trees.py
+++ src/Compocyte/core/models/trees.py
@@ -24,7 +24,9 @@ class BoostedTrees():
 
     def predict_logits(self, x) -> np.array:
         pred_activations = self.model.predict_proba(x)
-        self.model.cl
+        classes = self.model.classes_
+        self.labels_enc = {label: i for i, label in enumerate(classes)}
+        self.labels_dec = {self.labels_enc[label]: label for label in self.labels_enc.keys()}
             
         return pred_activations
 
