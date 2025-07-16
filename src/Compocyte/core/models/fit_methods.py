b753c8f9c884b751918c7d30ec8aae0bbafe3fe4
Christopher Beltz <sandmensch933@web.de>
2025-07-16 11:21:40 +0200

    Had to implement the logic here because pandas columns can't save multidimensional arrays

--- src/Compocyte/core/models/fit_methods.py
+++ src/Compocyte/core/models/fit_methods.py
@@ -65,8 +65,8 @@ def predict(model, x, threshold=-1, monte_carlo: int=None):
             else:
                 raise Exception('Unknown classifier type')
             
-        all_logits = np.array(all_logits).T
-        logits = np.mean(all_logits, axis=1)
+        all_logits = np.array(all_logits)
+        logits = np.mean(all_logits, axis=0)
     
     else:
         if isinstance(model, DenseTorch):
