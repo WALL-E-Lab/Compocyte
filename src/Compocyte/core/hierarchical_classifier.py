f3bee3de1ee25f42c3bb27d9cc15a07d7d414c12
Christopher Beltz <sandmensch933@web.de>
2025-08-05 09:18:00 +0200

    typo

--- src/Compocyte/core/hierarchical_classifier.py
+++ src/Compocyte/core/hierarchical_classifier.py
@@ -294,7 +294,7 @@ class HierarchicalClassifier(
             raise Exception(f'Features have already been selected at {node}.')
         
         subset = self.select_subset(node, max_cells=max_cells)
-        if hasattr(subset, 'todense'):
+        if hasattr(subset.X, 'todense'):
             subset.X = subset.X.todense()
             
         child_obs = self.obs_names[self.node_to_depth[node] + 1]
