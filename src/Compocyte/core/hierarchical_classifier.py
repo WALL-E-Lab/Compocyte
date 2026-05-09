fixed scaling axis

src/Compocyte/core/hierarchical_classifier.py
@@ -332,7 +332,7 @@ class HierarchicalClassifier(
         n_features = min(n_features, max_features)
 
         x = np.asarray(x)
-        x = robust_scale(x, axis=1, with_centering=False, copy=False, unit_variance=False)
+        x = robust_scale(x, axis=0, with_centering=False, copy=False, unit_variance=False)
         y = np.array(subset.obs[child_obs])
         selecter = SelectKBest(f_classif, k=n_features)
         selecter.fit(x, y)